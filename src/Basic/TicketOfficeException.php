01b4f127f62d51d80c1cb5a6bd36d5b141a14783
Francesco Face <francesco.face@gmail.com>
2016-09-26 20:39:05 +0200

    Adds base exception class TickeOfficeException

---
+++ src/Basic/TicketOfficeException.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Basic;
+
+class TicketOfficeException extends \Exception
+{
+    private $details;
+
+    public function toArray()
+    {
+        $a['error']['code'] = $this->getCode();
+        $a['error']['message'] = $this->getMessage();
+        $a['error']['details'] = $this->getDetails();
+        return $a;
+    }
+
+    public static function createWithDetails($details)
+    {
+        $e = new static();
+        $e->setDetails($details);
+        return $e;
+    }
+
+
+    public function toJson()
+    {
+        return json_encode($this->toArray());
+    }
+
+    public function setDetails($details)
+    {
+        $this->details = $details;
+    }
+
+    public function getDetails()
+    {
+        return $this->details;
+    }
+}
